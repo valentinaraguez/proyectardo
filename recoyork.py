1c4825879dbf6fc000c5ff35bfbe8ac40180547b
valentin <escvalentinaraguez@gmail.com>
2026-06-04 19:19:16 -0300

    Extensión de longitud de palabras, siendo de 3, 4, 5, 6  y 7

--- recoyork.py
+++ recoyork.py
@@ -11,15 +11,44 @@ def verificar_intento(palabra_secreta, intento):
             resultado.append("⬛")
     return resultado
 
-def jugar():
-    palabras = [
-        "sol", "mar", "pan", "luz", "ave", 
-        "rio", "sal", "pie", "mes", "dia",
-        "gol", "bar", "voz", "ley", "tos",
-        "uno", "sur", "fin", "mal", "dos"
-    ]
-    palabra_secreta = random.choice(palabras).upper()
-    intentos_maximos = 4
+    def jugar(longitud):
+        palabras ={
+         3: [
+         "sol", "mar", "pan", "luz", "ave", 
+         "rio", "sal", "pie", "mes", "dia",
+         "gol", "bar", "voz", "ley", "tos",
+         "uno", "sur", "fin", "mal", "dos"
+         ],
+
+        4: [
+            "gato","luna","rojo","azul","cafe",
+            "mesa","cama","pato","rana","nube",
+            "lago","cine","rayo","roca","sopa"
+        ],
+
+        5: [
+            "perro","arbol","negro","verde","playa",
+            "nieve","raton","mango","llave","piano",
+            "reloj","tigre","limon","queso","avion",
+            "cielo","campo","fruta","silla","radio"
+        ],
+
+        6: [
+            "tierra","viento","blanco","puerta","moneda",
+            "camino","espejo","jardin","bosque","piedra",
+            "fiesta","madera","helado","conejo","campos",
+            "tomate","estado","anillo","trapos","mantel"
+        ],
+
+        7: [
+            "ventana","caminos","monedas","pintura","jugador",
+            "mercado","escuela","palacio","botella","peligro",
+            "cerebro","trabajo","talento","pescado","abrazos",
+            "motores","musical","maestro","soldado","naranja"
+        ]
+    }
+    palabra_secreta = random.choice(palabras[longitud]).upper()
+    intentos_maximos = 5
     intentos = 0
 
     print("\n" + "=" * 30)
@@ -27,10 +56,10 @@ def jugar():
     print("=" * 30)
 
     while intentos < intentos_maximos:
-        intento = input("\ningresa una palabra (3 letras): ").upper().strip()
+        intento = input(f"\ningresa una palabra ({longitud} letras): ").upper().strip()
 
-        if len(intento) != 3 or not intento.isalpha():
-            print("x error: deben ser 3 letras")
+        if len(intento) != longitud or not intento.isalpha():
+            print(f"x error: deben ser {longitud} letras")
             continue
 
         resultado = verificar_intento(palabra_secreta, intento)
@@ -50,10 +79,16 @@ def jugar():
 
 def main():
     while True:
-        jugar()
+        opcion = input("\nelegi longitud de palabra (3 a 7): ").strip()
+        
+        if not opcion.isdigit() or int(opcion) not in [3, 4, 5, 6, 7]:
+            print("x opcion invalida")
+            continue
+
+        jugar(int(opcion))
         continuar = input("\njugar de nuevo? (s/n): ").lower().strip()
         if continuar not in ["s", "si", "sí"]:
-            print("\nnos vemos")
+            print("\nnos vemos chao pescao")
             break
 
 def mostrar_menu():
